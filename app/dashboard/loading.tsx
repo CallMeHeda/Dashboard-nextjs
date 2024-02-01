da3ba0c79e3f3df22d06b88f7be393b9596c8630
FatimaElamri <fatimaelamri789@gmail.com>
2024-02-01 14:02:19 +0100

    Chapter 9 - Streaming (skeleton)

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,9 +1,11 @@
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
+// import { ArrowPathIcon } from '@heroicons/react/24/outline';
+import DashboardSkeleton from '../ui/skeletons';
 
 export default function Loading() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <ArrowPathIcon className="h-24 w-24 animate-spin text-blue-500" />
-    </div>
+    <DashboardSkeleton />
+    // <div className="flex h-screen items-center justify-center">
+    //   <ArrowPathIcon className="h-24 w-24 animate-spin text-blue-500" />
+    // </div>
   );
 }
